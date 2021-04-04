99c4fd0f74820765db34df853ae123a74b00ebfb
Guest <guest-knliuq@h24604.sgb.skir.kr>
2021-04-04 09:37:37 +0900

    added word encoding sample file

---
+++ word_encoding.py
@@ -0,0 +1,40 @@
+
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = "A barber is a person. a barber is good person. a barber is huge person. he Knew A Secret! The Secret He Kept is huge secret. Huge secret. His barber kept his word.a barber kept his word. His barber kept his secret. But keeping and keeping such a huge secret to himself was driving the barber crazy. the barber went up a huge mountain."
+
+text = sent_tokenize(text)
+print(text)
+
+vocab = {}
+sentences = []
+stop_words = set(stopwords.words('english'))
+
+for i in text:
+    sentence = word_tokenize(i)
+    result = []
+
+    for word in sentences:
+        word = word.lower()
+        if word not in stop_words:
+            if len(word) > 2:
+               result.append(word)
+               if word not in vocab:
+                   vocab[word] = 0
+               vocab[word]+=1
+    sentences.append(result)
+print(sentences)
+
+
+vocab_sorted = sorted(vocab.items(),key= lambda x:x[1], reverse =True)
+
+word_to_index = {}
+i=0
+for (word,frequency) in vocab_sorted:
+    if frequency > 1:
+        i+=1
+        word_to_index[word] = i
+print(word_to_index)
+
